08f8cd3b8e7cbbbdc684ef78497f6a50c54b8ec1
PandaHans <123740257+PandaHans@users.noreply.github.com>
2024-05-08 15:38:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ url_file = open('urls.txt', "r")
 urls = url_file.readlines()
 
 def sleep_randomly():
-    delay = random.uniform(0.5, 1.5)
+    delay = random.uniform(0.1, 0.5)
     time.sleep(delay)
 
 time.sleep(5)
@@ -46,17 +46,26 @@ def scroll_into_view(driver, element):
     driver.execute_script("window.scrollBy(0, -100);")
 
 # Function to scroll down the page until there are no more elements with the specified class name
-def scroll_until_no_more_elements(driver, class_name, max_scrolls):
-    for _ in range(max_scrolls):
-        if are_more_elements_present(driver, class_name):
-            # Scroll down to the bottom of the page
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            print("Scrolling down")
-            # Wait for a brief moment for content to load
-            time.sleep(2)
-        else:
+def scroll_until_no_more_elements(driver, class_name):
+    # Get the initial number of elements with the specified class name
+    initial_count = len(driver.find_elements(By.CLASS_NAME, class_name))
+    
+    while True:
+        # Scroll down to the bottom of the page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        print("Scrolling down" + str(initial_count))
+        # Wait for a brief moment for content to load
+        time.sleep(2)
+        
+        # Get the current number of elements with the specified class name
+        current_count = len(driver.find_elements(By.CLASS_NAME, class_name))
+        
+        # If the number of elements has not increased, break the loop
+        if current_count == initial_count:
             break
-
+        
+        # Update the initial count
+        initial_count = current_count
 
 for url in urls:
     print('--Liking comments for this post: ' + url)
@@ -64,13 +73,13 @@ for url in urls:
     driver.get(url)
 
     try:
-        
+        count = 0
 
         # Wait for the like buttons to be clickable
         WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'css-gb2mrc-SpanCount.ezxoskx3')))
 
         # Scroll down to load all comments
-        scroll_until_no_more_elements(driver, 'css-gb2mrc-SpanCount.ezxoskx3', max_scrolls=10)  # Adjust the max_scrolls as needed
+        scroll_until_no_more_elements(driver, 'css-gb2mrc-SpanCount.ezxoskx3')  # Adjust the max_scrolls as needed
 
         # Find all like buttons
         like_buttons = driver.find_elements(By.CLASS_NAME, 'css-gb2mrc-SpanCount.ezxoskx3')
@@ -78,13 +87,13 @@ for url in urls:
         # Click on each like button
         for button in like_buttons:
             scroll_into_view(driver, button)
-            time.sleep(1)
+            time.sleep(0.3)
             button.click()
-            print("liked")
+            count += 1
+            print("liked" + str(count))
             sleep_randomly()
 
     except NoSuchElementException:
         print('Could not find like buttons.')
 
 print('FINISHED')
-driver.quit()
